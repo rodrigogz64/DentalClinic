6f8db158ad8a06e28bc55e653a7e3791558afbab
Rodrigo Grande <96559980+rodrigogz64@users.noreply.github.com>
2023-09-15 21:28:23 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState()
+
+  const getData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/');
+      const data = await response.json();
+      setData(data);
+    } catch (error) { console.error('Error', error); }
+  }
+
+  useEffect( () => { getData() }, [])
+
+  return (
+    <p>{JSON.stringify(data, null, 2)}</p>
+  )
+}
+
+export default App
